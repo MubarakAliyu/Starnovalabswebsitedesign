0084046bd1c4d95b657229663a1d718f7e6830be
Mubarak Aliyu <mubeegraphics@gmail.com>
2026-02-18 11:04:57 +0100

    prepare for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/Starnovalabswebsitedesign/',   // <-- ADD THIS LINE
+
   plugins: [
     react(),
     tailwindcss(),
